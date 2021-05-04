6c853ed68cae9ecb83d6939f19f6e38ec0461c03
jieunjeong <qpapple@naver.com>
2021-05-04 22:46:09 +0900

    - gitignore add - created babel

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # learn-babel
+## Babel(v7.13)
+
+###### 참고 사이트
+- https://babeljs.io/
+- https://www.npmjs.com/
+
+
+### 설치 & 실행
+- $ npm install
+- $ npm run build
---
+++ index.js
@@ -0,0 +1,5 @@
+// Run "npm start" on the terminal
+import path from "path"
+const outputPath = path.resolve(__dirname, "dist")
+console.log(`Output path is "${outputPath}".`)
+export { outputPath }
